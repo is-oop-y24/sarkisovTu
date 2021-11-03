de1e83696e24eaa4e3e5463a419b995c6fe7d2fb
sarkisovTu <sarkisov1337@gmail.com>
2021-11-03 18:26:11 +0300

    fix: resolve branch file conflicts

--- Isu.Tests/IsuServiceTest.cs
+++ Isu.Tests/IsuServiceTest.cs
@@ -1,3 +1,4 @@
+using Isu.Models;
 using Isu.Services;
 using Isu.Tools;
 using NUnit.Framework;
@@ -6,6 +7,52 @@ namespace Isu.Tests
 {
     public class Tests
     {
+        private IIsuService _isuService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _isuService = new IsuService();
+        }
+
+        [Test]
+        public void AddStudentToGroup_StudentHasGroupAndGroupContainsStudent()
+        {
+            Group m3101 = _isuService.AddGroup("M3101");
+            _isuService.AddStudent(m3101, "Name Surname");
+            Assert.Pass();
+        }
+
+        [Test]
+        public void ReachMaxStudentPerGroup_ThrowException()
+        {
+            Group m3101 = _isuService.AddGroup("M3101");
+            
+            Assert.Catch<IsuException>(() =>
+            {
+                //max student number in group is 35. Try to add 4o students in one group
+                for (int i = 0; i < 40; i++)
+                {
+                    _isuService.AddStudent(m3101, $"Student{i}");
+                }
+            });
+        }
+
+        [Test]
+        public void CreateGroupWithInvalidName_ThrowException()
+        {
+            //group name template: M3XYY, X - course number, YY - group number
+            Assert.Catch<IsuException>(() =>
+            {
+                _isuService.AddGroup("13201");
+            });
+        }
         
+        [Test]
+        public void CompareGroupsByReference_GroupsAreEqual()
+        {
+            Group group = _isuService.AddGroup("M3208");
+            Assert.True(group.Equals(_isuService.FindGroup("M3208")));
+        }
     }
 }
--- Isu/Services/IIsuService.cs
+++ Isu/Services/IIsuService.cs
@@ -1,6 +1,21 @@
-﻿namespace Isu.Services
+﻿using System.Collections.Generic;
+using Isu.Models;
+using Group = Isu.Models.Group;
+
+namespace Isu.Services
 {
     public interface IIsuService
     {
+        Group AddGroup(string name);
+        Student AddStudent(Group group, string name);
+
+        Student GetStudent(int id);
+        Student FindStudent(string name);
+        List<Student> FindStudents(string groupName);
+        List<Student> FindStudents(CourseNumber courseNumber);
+        Group FindGroup(string groupName);
+        List<Group> FindGroups(CourseNumber courseNumber);
+
+        void ChangeStudentGroup(Student student, Group newGroup);
     }
 }
